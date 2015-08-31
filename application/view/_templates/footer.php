cc126253b15f6e036506c6c8efd98e19148147c3
hzh <hzh0512@gmail.com>
2015-08-31 15:38:23 +0800

    修改footer

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -5,6 +5,6 @@
     <hr/>
     <div class="footdiv"><a href="<?php echo URL;?>forum/listall/1">平台报错</a>&nbsp;
     <a href="mailto:xxxxx@thedc.org" class="email" title="联系邮箱">xxxxx@thedc.org</a>
-    <br>designed by 队式网站组</div>
+    <br>designed by 电设网站组</div>
 </body>
 </html>
